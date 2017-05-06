5fdf76b8b61a48c4520e9ec68f97fbfa6fd57970
Owais Hussain <owais.hussain@ihsinformatics.com>
2017-05-06 13:53:10 +0500

    - Mavenized the project - Added Quartz jobs for Sms and Calls - Successfully made SMS calls - Added project, classpath and gitignore files

---
+++ src/com/ihsinformatics/gfatmnotifications/CallNotificationsJob.java
@@ -0,0 +1,84 @@
+/* Copyright(C) 2017 Interactive Health Solutions, Pvt. Ltd.
+
+This program is free software; you can redistribute it and/or modify it under the terms of the GNU General Public License as
+published by the Free Software Foundation; either version 3 of the License (GPLv3), or any later version.
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+
+See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program; if not, write to the Interactive Health Solutions, info@ihsinformatics.com
+You can also access the license on the internet at the address: http://www.gnu.org/licenses/gpl-3.0.html
+
+Interactive Health Solutions, hereby disclaims all copyright interest in this program written by the contributors.
+*/
+
+package com.ihsinformatics.gfatmnotifications;
+
+import java.util.Date;
+
+import org.quartz.Job;
+import org.quartz.JobExecutionContext;
+import org.quartz.JobExecutionException;
+
+import com.ihsinformatics.util.DatabaseUtil;
+
+/**
+ * @author owais.hussain@ihsinformatics.com
+ *
+ */
+public class CallNotificationsJob implements Job {
+
+	private DatabaseUtil localDb;
+	private DatabaseUtil remoteDb;
+
+	private boolean filterDate = true;
+	private Date dateFrom;
+	private Date dateTo;
+
+	/* (non-Javadoc)
+	 * @see org.quartz.Job#execute(org.quartz.JobExecutionContext)
+	 */
+	public void execute(JobExecutionContext context)
+			throws JobExecutionException {
+		// TODO Auto-generated method stub
+
+	}
+
+	public DatabaseUtil getLocalDb() {
+		return localDb;
+	}
+
+	public void setLocalDb(DatabaseUtil localDb) {
+		this.localDb = localDb;
+	}
+
+	public DatabaseUtil getRemoteDb() {
+		return remoteDb;
+	}
+
+	public void setRemoteDb(DatabaseUtil remoteDb) {
+		this.remoteDb = remoteDb;
+	}
+
+	public boolean isFilterDate() {
+		return filterDate;
+	}
+
+	public void setFilterDate(boolean filterDate) {
+		this.filterDate = filterDate;
+	}
+
+	public Date getDateFrom() {
+		return dateFrom;
+	}
+
+	public void setDateFrom(Date dateFrom) {
+		this.dateFrom = dateFrom;
+	}
+
+	public Date getDateTo() {
+		return dateTo;
+	}
+
+	public void setDateTo(Date dateTo) {
+		this.dateTo = dateTo;
+	}
+}
